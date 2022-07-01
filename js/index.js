9be648889b2714ed3d6f99044e7569aa359bba79
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-01 16:56:09 +0530

    Complete theme clock project

--- js/index.js
+++ js/index.js
@@ -24,17 +24,19 @@ function setTime() {
   const time = new Date();
   const month = time.getMonth();
   const day = time.getDay();
+  const date = time.getDate();
   const hours = time.getHours();
-
   const hoursForClock = hours % 12
   const minutes = time.getMinutes()
   const seconds = time.getSeconds()
+  const ampm = hours >= 12 ? ('PM') : ('AM')
 
   hourEL.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
   minuteEL.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
   secondEL.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 
-  // console.log(time)
+  timeEL.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  dateEL.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
 
