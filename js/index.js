32d68e9c1797d338876427d3a882898e9983f731
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-01 15:02:23 +0530

    1. Decleare variable on js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 const hourEL = document.querySelector('.hour')
+const minuteEL = document.querySelector('.minute')
+const secondEL = document.querySelector('.second')
+const timeEL = document.querySelector('.time')
+const dateEL = document.querySelector('.date')
+const toggleEL = document.querySelector('.toggle')
+
