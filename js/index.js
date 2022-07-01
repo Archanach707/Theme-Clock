6785261d3da3cff6244272dff273d82eae489e70
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-01 15:23:09 +0530

    1. Button is toggle by js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const minuteEL = document.querySelector('.minute')
 const secondEL = document.querySelector('.second')
 const timeEL = document.querySelector('.time')
 const dateEL = document.querySelector('.date')
-const toggleEL = document.querySelector('.toggle')
+const toggle = document.querySelector('.toggle')
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const months = ["Jan", "Fab", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -14,7 +14,8 @@ toggle.addEventListener('click', (e) => {
     html.classList.remove('dark')
     e.target.innerHTML = 'Dark mode'
   } else {
-    
+    html.classList.add('dark')
+    e.target.innerHTML = 'Light mode'
   }
 
 })
